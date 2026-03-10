4e3d87d73ae4f742ace6e71bb22f8676159d7642
Jiashu Li <452472803ljs@gmail.com>
2026-03-10 09:58:18 -0400

    Initial Commit

---
+++ BetterLife/BetterLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterLifeApp.swift
+//  BetterLife
+//
+//  Created by Jason Li on 2026-03-10.
+//
+
+import SwiftUI
+
+@main
+struct BetterLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
